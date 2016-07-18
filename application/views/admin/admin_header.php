e4b1056b89db29dcc595c83191d73773a53f516b
pila <pila_dil@yahoo.com>
2016-07-18 10:36:35 +0545

    corrected the logout redirection

--- application/views/admin/admin_header.php
+++ application/views/admin/admin_header.php
@@ -19,7 +19,7 @@
         <button type="submit" class="btn btn-primary">Submit</button>
       </form>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="login/logout">Logout</a></li>
+          <li><a href=<?= base_url('login/logout')?> >Logout</a></li>
       </ul>
     </div>
   </div>
